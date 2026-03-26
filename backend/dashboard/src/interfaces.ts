141db257bea2cfd3560d520518534b776cad62b2
chi828 <124188695+chi828@users.noreply.github.com>
2026-03-26 14:13:01 +0100

    Add dashboard interactor stubs & interfaces

---
+++ backend/dashboard/src/interfaces.ts
@@ -0,0 +1,31 @@
+import type { Advertisement, Coordinates } from "@dieti-estates-2025/entities";
+import { AdvertisementsMetrics, AdvertisementData } from "./dataObjects.js";
+
+export interface DetectPOIsService {
+    detectPOIs(location: Coordinates): string[];
+}
+
+export interface CreateNewAdvertisementPresenter {
+    present(id: number): void;
+    presentError(error: Error): void;
+}
+
+export interface AdvertisementRepository {
+    readAdvertisementById(id: number): Advertisement;
+    readAllAdvertisements(): Advertisement[];
+    createAdvertisement(advertisement: Advertisement): Advertisement;
+    updateAdvertisement(advertisement: Advertisement): Advertisement;
+}
+
+export interface RetrieveAdvertisementsDataPresenter {
+    present(data: AdvertisementData): void;
+    presentError(error: Error): void;
+}
+
+export interface AdvertisementMetricsRepository {
+    getAdvertisementsMetrics(): AdvertisementsMetrics;
+    increaseOfferCounter(id: number): void;
+    increaseViewCounter(id: number): void;
+    increasePrenotationCounter(id: number): void;
+    markAsTaken(id: number): void;
+}
