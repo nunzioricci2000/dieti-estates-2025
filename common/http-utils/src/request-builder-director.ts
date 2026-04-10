dd46e99f2d9b758312394cf200fd427a28bf3f88
chi828 <124188695+chi828@users.noreply.github.com>
2026-04-10 14:47:43 +0200

    RequestBuilderDirector implemented

--- common/http-utils/src/request-builder-director.ts
+++ common/http-utils/src/request-builder-director.ts
@@ -1,7 +1,14 @@
 import type { RequestBuilder } from "./request-builder.js";
+import type { Request } from "./request.js";
 
 export class RequestBuilderDirector {
     makeRequest(builder: RequestBuilder): Request {
-        throw new Error("To be implemented");
+        builder.setMethod();
+        builder.setPath();
+        builder.setBody();
+        builder.setPathParams();
+        builder.setQueryParams();
+        builder.setHeaders();
+        return builder.getResult();
     }
 }
