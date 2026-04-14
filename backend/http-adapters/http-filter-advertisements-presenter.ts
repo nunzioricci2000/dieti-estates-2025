3c65192b92c9be845b2549b602ae75e3b3638496
chi828 <124188695+chi828@users.noreply.github.com>
2026-04-14 12:47:45 +0200

    Update http-filter-advertisements-presenter.ts

--- backend/http-adapters/http-filter-advertisements-presenter.ts
+++ backend/http-adapters/http-filter-advertisements-presenter.ts
@@ -1,7 +1,9 @@
 import type { Logger } from "@dieti-estates-2025/utilities";
 import type { FilterAdvertisementsPresenter } from "../user/src/interfaces.js";
 import type { ResponseManager } from "./response-manager.js";
-import type { Advertisement } from "@dieti-estates-2025/entities";
+import { Rental, Sale, type Advertisement } from "@dieti-estates-2025/entities";
+import type { AdvertisementDTO } from "../../common/http-utils/src/dto.js";
+import { Response } from "../../common/http-utils/src/response.js";
 
 export class HTTPFilterAdvertisementsPresenter implements FilterAdvertisementsPresenter {
     constructor(
@@ -11,11 +13,44 @@ export class HTTPFilterAdvertisementsPresenter implements FilterAdvertisementsPr
         logger.info("Created!");
     }
 
-    present(advertisement: Advertisement[]): void {
-        // TODO implement
+    present(advertisements: Advertisement[]): void {
+        const body: AdvertisementDTO[] = [];
+        for (const ad of advertisements) {
+            body.push({
+                id: ad.id,
+                address: ad.address,
+                city: ad.city,
+                coordinates: {
+                    latitude: ad.location.latitude,
+                    longitude: ad.location.longitude,
+                },
+                images: ad.images.map(image => image.url),
+                dimensions: ad.dimensions,
+                description: ad.description,
+                numberOfRooms: ad.numberOfRooms,
+                energyClass: ad.energyClass,
+                additionalServices: ad.additionalServices,
+                nearbyPOIs: ad.nearbyPOIs,
+                kind: ad instanceof Sale ? "sale" :
+                    "rent",
+                price: ad instanceof Sale ? ad.price.value :
+                    (ad as Rental).rentPrice.price.value,
+            })
+        }
+
+        const headers = new Map<string, string>();
+
+        const res = new Response(
+            200,
+            body,
+            headers,
+        )
+
+        this.responseManager.sendResponse(res);
     }
 
     presentError(error: Error): void {
-        // TODO IMplement
+        this.responseManager.sendError(error);
+        this.logger.error("Error!");
     }
 }
